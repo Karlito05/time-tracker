33b7f73f9cf1996935db5162911073e7eca8729e
Karel Lukes <karelos101@gmail.com>
2026-07-18 18:45:39 +0200

    fixed a bug where app would overflow the screen

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -8,7 +8,7 @@ export default async function Page() {
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-start py-32 px-48 min-w-2xl bg-white dark:bg-black sm:items-start">
+      <main className="flex flex-1 w-full max-w-sm flex-col items-center justify-start md:py-32 md:max-w-3xl md:px-48 py-6 px-6 bg-white dark:bg-black sm:items-start">
         <Overview activitiesTimes={activitiesTimes} />
       </main>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default async function Page() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-48 min-w-2xl bg-white dark:bg-black ">
+      <main className="flex flex-1 w-full max-w-full flex-col items-center justify-center py-32 px-48 min-w-2xl bg-white dark:bg-black ">
         <Link href={"/timer"}>Continue to the app</Link>
       </main>
     </div>
